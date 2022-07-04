feb0cc774ac5fcc8cdc1458576c1aac74be8a44a
Alex Meireles <alexmeirelesalmeida@hotmail.com>
2022-07-04 02:46:01 -0300

    Added the car traveling the route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import {
+  getStopPointCoordinate,
+  getGpsPosition,
+  getGpsLength,
+} from "./utils/dataHandler";
 
-
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import { getStopPointCoordinate, getGpsPosition, getGpsLength } from './utils/dataHandler'
-
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleG1laXJlbHMiLCJhIjoiY2w1MGF0YzZwMGtxaTNwcWwwbjFjZWxldCJ9.NuJ9ynxJsImQYRschzqUkg';
-
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWxleG1laXJlbHMiLCJhIjoiY2w1MGF0YzZwMGtxaTNwcWwwbjFjZWxldCJ9.NuJ9ynxJsImQYRschzqUkg";
 
 function App() {
-  
   const points = getStopPointCoordinate(0).map((e) => {
-    return {long:e[0], late:e[1]}  
-  })
-
-  
+    return { long: e[0], late: e[1] };
+  });
 
   const mapContainer = useRef(null);
   const map = useRef(null);
@@ -23,96 +21,102 @@ function App() {
   const [zoom, setZoom] = useState(9);
   const [time, setTime] = useState(0);
 
-  
   const geojson = {
-    type: 'FeatureCollection',
+    type: "FeatureCollection",
     features: [
       {
-        type: 'Feature',
+        type: "Feature",
         geometry: {
-          type: 'Point',
-          coordinates: [points[0].long, points[0].late]
+          type: "Point",
+          coordinates: [points[0].long, points[0].late],
         },
         properties: {
-          title: 'Mapbox',
-          description: 'Initial'
-        }
+          title: "Mapbox",
+          description: "Initial",
+        },
       },
       {
-        type: 'Feature',
+        type: "Feature",
         geometry: {
-          type: 'Point',
-          coordinates: [points[1].long, points[1].late]
+          type: "Point",
+          coordinates: [points[1].long, points[1].late],
         },
         properties: {
-          title: 'Mapbox',
-          description: 'End'
-        }
-      }
-    ]
+          title: "Mapbox",
+          description: "End",
+        },
+      },
+    ],
   };
 
   useEffect(() => {
     setInterval(() => {
-      if(time < getGpsLength(0))
-        setTime(prevCount => prevCount + 1);
-      
+      if (time < getGpsLength(0)) setTime((prevCount) => prevCount + 1);
     }, 1000);
   }, []);
 
   useEffect(() => {
-    if(getGpsPosition(0, time)){
+    //console.log("entrou");
+    //console.log(time);
+    if (getGpsPosition(0, time)) {
+      const el = document.createElement("div");
+      el.className = "marker";
+      el.id = `marker${time}`;
+
+      if (time >= 1 && time < getGpsLength(0)) {
+        document.getElementById(`marker${time - 1}`).style.display = "none";
+      }
+
       const newPosition = {
         type: "NewPosition",
-        coordinates: [getGpsPosition(0, time).longitude, getGpsPosition(0, time).latitude]
-  
-      }
-      console.log(newPosition.coordinates)
-      // mudar marcação do carro.
-  
-       map.current && new mapboxgl.Marker().setLngLat(newPosition.coordinates).addTo(map.current);
+        coordinates: [
+          getGpsPosition(0, time).longitude,
+          getGpsPosition(0, time).latitude,
+        ],
+      };
+      map.current &&
+        new mapboxgl.Marker(el)
+          .setLngLat(newPosition.coordinates)
+          .addTo(map.current);
     }
-    
-  }, [time])
-  
+  }, [time]);
+
   //console.log(points[0].long, points[0].late)
 
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
+    map.current.on("move", () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
-  },);
+  });
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: "mapbox://styles/mapbox/streets-v11",
       center: [lng, lat],
       zoom: zoom,
     });
-    
   });
 
   useEffect(() => {
     for (const feature of geojson.features) {
-      new mapboxgl.Marker().setLngLat(feature.geometry.coordinates).addTo(map.current);
+      new mapboxgl.Marker()
+        .setLngLat(feature.geometry.coordinates)
+        .addTo(map.current);
     }
-  }, [])
-  
-  
+  }, []);
 
   return (
-      <div>
-        <div className="sidebar">
-          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-        </div>
-        <div ref={mapContainer} className="map-container" />
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
-    
+      <div ref={mapContainer} className="map-container" />
+    </div>
   );
 }
 
