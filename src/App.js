03674769043e8d0ef7af86ff1836798bf6ec593d
Alex Meireles <alexmeirelesalmeida@hotmail.com>
2022-07-05 10:34:07 -0300

    Added new routes with your markings

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {
   getStopPointCoordinate,
   getGpsPosition,
   getGpsLength,
+  getPoints,
+  getGeoJson,
 } from "./utils/dataHandler";
 
 import { useTranslation } from "react-i18next";
@@ -12,84 +14,71 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoiYWxleG1laXJlbHMiLCJhIjoiY2w1MGF0YzZwMGtxaTNwcWwwbjFjZWxldCJ9.NuJ9ynxJsImQYRschzqUkg";
 
 function App() {
-  const { t, i18n } = useTranslation();
-
-  const changeLanguage = (language) => {
-    i18n.changeLanguage(language);
-  };
-
-  const points = getStopPointCoordinate(0).map((e) => {
-    return { long: e[0], late: e[1] };
-  });
-
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(points[0].long);
-  const [lat, setLat] = useState(points[0].late);
-  const [zoom, setZoom] = useState(9);
+  const [points, setPoints] = useState(getPoints(0));
+  const [lng, setLng] = useState(getPoints(0)[0].long);
+  const [lat, setLat] = useState(getPoints(0)[0].late);
+  const [zoom, setZoom] = useState(12);
   const [time, setTime] = useState(0);
-
-  const geojson = {
-    type: "FeatureCollection",
-    features: [
-      {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [points[0].long, points[0].late],
-        },
-        properties: {
-          title: "Mapbox",
-          description: "Initial",
-        },
-      },
-      {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [points[1].long, points[1].late],
-        },
-        properties: {
-          title: "Mapbox",
-          description: "End",
-        },
-      },
-    ],
-  };
+  const [routes, setRoutes] = useState(0);
+  const [geoJson, setGeoJson] = useState(getGeoJson(0));
+  const [execution, setExcution] = useState(0);
 
   useEffect(() => {
-    setInterval(() => {
-      if (time < getGpsLength(0)) setTime((prevCount) => prevCount + 1);
-    }, 1000);
+    // seta as novas rotas e remove os carrinhos da rota anterior
+    const currentTime = time;
+    let counterRemover = 0;
+    while (counterRemover < 10) {
+      const olderPosition = document.getElementById(
+        `marker${currentTime - counterRemover}-${execution - 1}`
+      );
+      if (olderPosition)
+        document.getElementById(
+          `marker${currentTime - counterRemover}-${execution - 1}`
+        ).style.display = "none";
+      counterRemover = counterRemover + 1;
+    }
+    setLng(getPoints(routes)[0].long);
+    setLat(getPoints(routes)[0].late);
+    setTime(0);
+    setGeoJson(getGeoJson(routes));
+  }, [routes]);
+
+  useEffect(() => {
+    const counter = setInterval(() => {
+      setTime((prevCount) => prevCount + 1);
+    }, 2000);
   }, []);
 
   useEffect(() => {
-    //console.log("entrou");
-    //console.log(time);
-    if (getGpsPosition(0, time)) {
+    // remove a ultima aparição e adiciona a nova.
+    if (getGpsPosition(routes, time)) {
       const el = document.createElement("div");
       el.className = "marker";
-      el.id = `marker${time}`;
+      el.id = `marker${time}-${execution}`;
 
-      if (time >= 1 && time < getGpsLength(0)) {
-        document.getElementById(`marker${time - 1}`).style.display = "none";
+      if (time >= 1 && time < getGpsLength(routes)) {
+        const olderPosition = document.getElementById(
+          `marker${time - 1}-${execution}`
+        );
+        if (olderPosition)
+          document.getElementById(
+            `marker${time - 1}-${execution}`
+          ).style.display = "none";
       }
 
-      const newPosition = {
-        type: "NewPosition",
-        coordinates: [
-          getGpsPosition(0, time).longitude,
-          getGpsPosition(0, time).latitude,
-        ],
-      };
+      const newPointCoordinates = [
+        getGpsPosition(routes, time).longitude,
+        getGpsPosition(routes, time).latitude,
+      ];
+
       map.current &&
         new mapboxgl.Marker(el)
-          .setLngLat(newPosition.coordinates)
+          .setLngLat(newPointCoordinates)
           .addTo(map.current);
     }
   }, [time]);
-
-  //console.log(points[0].long, points[0].late)
 
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
@@ -111,12 +100,33 @@ function App() {
   });
 
   useEffect(() => {
-    for (const feature of geojson.features) {
-      new mapboxgl.Marker()
+    // adiciona as marcas inicial e final de cada rota
+    let positionCount = 0;
+    for (const feature of geoJson.features) {
+      const currentMarker = new mapboxgl.Marker({
+        color: positionCount > 0 ? "red" : "blue",
+      })
         .setLngLat(feature.geometry.coordinates)
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+          )
+        )
         .addTo(map.current);
+      positionCount = positionCount + 1;
     }
-  }, []);
+  }, [geoJson]);
+
+  const changeRoute = (route) => {
+    setRoutes(route);
+    setExcution(execution + 1);
+  };
+
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
 
   return (
     <div>
@@ -128,12 +138,16 @@ function App() {
         <button onClick={() => changeLanguage("en")}>EN</button>
         <button onClick={() => changeLanguage("pt")}>PT</button>
         <button onClick={() => changeLanguage("es")}>ES</button>
+        <div>{t("description.part1")}</div>
         <hr />
         <div>
           <h1>{t("title")}</h1>
+          <button onClick={() => changeRoute(1)}>{t("routes.routes1")}</button>
+          <button onClick={() => changeRoute(2)}>{t("routes.routes2")}</button>
+          <button onClick={() => changeRoute(3)}>{t("routes.routes3")}</button>
+          <button onClick={() => changeRoute(4)}>{t("routes.routes4")}</button>
+          <button onClick={() => changeRoute(5)}>{t("routes.routes5")}</button>
         </div>
-        <div>{t("description.part1")}</div>
-        <div>{t("description.part2")}</div>
       </div>
     </div>
   );
--- src/utils/dataHandler.js
+++ src/utils/dataHandler.js
@@ -1,14 +1,49 @@
-import data from '../data/frontend_data_gps.json'
+import data from "../data/frontend_data_gps.json";
 
 export const getStopPointCoordinate = (index) => {
-    return data.courses[index].stop_points.coordinates
-}
+  return data.courses[index].stop_points.coordinates;
+};
 
 export const getGpsPosition = (index, time) => {
-    return data.courses[index].gps[time]
-}
+  return data.courses[index].gps[time];
+};
 
 export const getGpsLength = (index) => {
-    return data.courses[index].gps.length
-}
+  return data.courses[index].gps.length;
+};
 
+export const getPoints = (routes) => {
+  return getStopPointCoordinate(routes).map((e) => {
+    return { long: e[0], late: e[1] };
+  });
+};
+
+export const getGeoJson = (routes) => {
+  return {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [getPoints(routes)[0].long, getPoints(routes)[0].late],
+        },
+        properties: {
+          title: `Route ${routes}`,
+          description: "Start",
+        },
+      },
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [getPoints(routes)[1].long, getPoints(routes)[1].late],
+        },
+        properties: {
+          title: `Route ${routes}`,
+          description: "End",
+        },
+      },
+    ],
+  };
+};
